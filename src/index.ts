07488736a1ebc7c19dec2e256ce06277459813fc
FE92star <1737765939@qq.com>
2022-09-28 17:31:52 +0800

    feat: 更新vscode相关配置

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import { window } from 'vscode'
+import type { ExtensionContext } from 'vscode'
+import { commands, window } from 'vscode'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+export function activate(ctx: ExtensionContext) {
+  const disposable = commands.registerCommand('extension.helloWorld', () => {
+    window.showInformationMessage('Hello World')
+  })
+
+  ctx.subscriptions.push(disposable)
 }
 
 export function deactivate() {
